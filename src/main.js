adeec722cd2b4e04669c14ba81557097ab04dbd2
Léo Boyer <contact@leoboyer.fr>
2022-05-28 15:15:00 +0200

    demo scene

--- src/main.js
+++ src/main.js
@@ -9,9 +9,12 @@ app.use(static('public'))
 app.get('/screenshot.png', async (req, res) => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
+    page
+        .on('console', message =>
+            console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
     await page.goto(`http://localhost:${port}`)
     const data = await page.screenshot({ encoding : 'base64', type: 'png' } )
-    console.log(data)
+
     res.contentType('image/png');
     res.end(data, 'base64');
     await browser.close()
