e1dfae2c630f436fb5fb856b461c19d08c8a243d
Léo Boyer <contact@leoboyer.fr>
2022-05-28 14:01:40 +0200

    add puppeteer

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const {static} = require('express')
+const puppeteer = require('puppeteer')
 const app = express()
 const port = 1556
 
 app.use(static('public'))
 
 app.get('/screenshot.png', async (req, res) => {
-    // const browser = await puppeteer.launch()
-    // const page = await browser.newPage()
-    // await page.goto(`http://localhost:${port}`)
-    // const data = await page.screenshot({ encoding : 'base64', type: 'png' } )
-    // console.log(data)
-    // res.contentType('image/png');
-    // res.end(data, 'base64');
-    // await browser.close()
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(`http://localhost:${port}`)
+    const data = await page.screenshot({ encoding : 'base64', type: 'png' } )
+    console.log(data)
+    res.contentType('image/png');
+    res.end(data, 'base64');
+    await browser.close()
 })
 
 app.listen(port, () => {
